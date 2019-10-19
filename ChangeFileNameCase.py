1295c9256b8bb0f356404ca81f465775433a4654
Ben Levy <contact@benlevy.com>
2019-10-18 21:32:49 -0500

    Upgrades for python 3

--- ChangeFileNameCase.py
+++ ChangeFileNameCase.py
@@ -1,12 +1,11 @@
-from Tkinter import Tk
-from tkFileDialog import askdirectory
+from tkinter import *
+from tkinter import filedialog
 
 import os
 import sys
 
 if len(sys.argv) < 2:
-	Tk().withdraw()
-	path = askdirectory()
+	path = filedialog.askdirectory()
 else:
 	path = sys.argv[1]
 
@@ -14,6 +13,6 @@ try:
 	for f in os.listdir(path):
 		if os.path.isfile(os.path.join(path, f)):
 			os.rename(os.path.join(path, f), os.path.join(path, f.lower()))
-	print "Renamed files in " + path + " to lowercase"
-except OSError, e:
-	print e
+	print("Renamed files in " + path + " to lowercase")
+except OSError as e:
+	print(e)
